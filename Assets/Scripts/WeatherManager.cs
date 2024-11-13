1e51ef5e0860ee9bf36dd7ddab66597e4fc674fa
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2024-11-13 15:03:15 -0500

    WeatherProfile Addition

--- Assets/Scripts/WeatherManager.cs
+++ Assets/Scripts/WeatherManager.cs
@@ -8,7 +8,7 @@ public class WeatherManager : MonoBehaviour
 {
 
     public int townID;
-    public Vector2[] towns;
+    public string[] towns;
     private int currentTownID;
 
     public float autoUpdateRate = 5 * 60 * 60;
@@ -35,11 +35,15 @@ public class WeatherManager : MonoBehaviour
 
     }
 
-    public void UpdateWeather() => StartCoroutine(CallAPI(xmlApi, null));
 
 
-    //Unknown functionality
-    private const string xmlApi = "http://api.openweathermap.org/data/2.5/weather?q=Orlando,us&mode=xml&appid=APIKEY";
+    private const string APIKEY = "c7ce360dc9c6e5c1353d0aaa5ab7a187";
+
+    public void UpdateWeather()
+    {
+        string value = $"http://api.openweathermap.org/data/2.5/weather?q={towns[townID]}&mode=json&appid={APIKEY}";
+        StartCoroutine(CallAPI(value, null));
+    }
 
     private IEnumerator CallAPI(string url, Action<string> callback)
     {
@@ -61,9 +65,5 @@ public class WeatherManager : MonoBehaviour
         }
     }
 
-    public IEnumerator GetWeatherXML(Action<string> callback)
-    {
-        return CallAPI(xmlApi, callback);
-    }
 
 }
